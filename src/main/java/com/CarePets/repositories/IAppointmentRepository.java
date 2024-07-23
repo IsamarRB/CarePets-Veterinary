959b09d9d9ae6dc2f848104b631caf3f64bf675c
Marta Gonzalez Morales <pythonisamgm@gmail.com>
2024-07-23 15:23:32 +0200

    feat: add methods in irepository to create specific read methods

--- src/main/java/com/CarePets/repositories/IAppointmentRepository.java
+++ src/main/java/com/CarePets/repositories/IAppointmentRepository.java
@@ -3,5 +3,11 @@ package com.CarePets.repositories;
 import com.CarePets.models.Appointment;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface IAppointmentRepository extends CrudRepository<Appointment,Long> {
+   Appointment findByName(String name);
+   Appointment findByTypeConsult(String typeConsult);
+    Appointment findByStatus(String status);
+
 }
