0c0c1196e9728825c29d588e25467fcc1f1cd4e0
Marta Gonzalez Morales <pythonisamgm@gmail.com>
2024-07-24 15:10:30 +0200

    feat: add isAvailable method for LocalDateTime

--- src/main/java/com/CarePets/repositories/IAppointmentRepository.java
+++ src/main/java/com/CarePets/repositories/IAppointmentRepository.java
@@ -3,11 +3,13 @@ package com.CarePets.repositories;
 import com.CarePets.models.Appointment;
 import org.springframework.data.repository.CrudRepository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 public interface IAppointmentRepository extends CrudRepository<Appointment,Long> {
    Appointment findByName(String name);
    Appointment findByTypeConsult(String typeConsult);
-    Appointment findByStatus(String status);
+   Appointment findByStatus(String status);
+   boolean isDateAvailable(LocalDateTime localDateTime);
 
 }
