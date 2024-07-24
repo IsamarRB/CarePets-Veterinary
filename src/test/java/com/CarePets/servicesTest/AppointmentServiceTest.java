88901d7336c5dfcfd532514c64a4cf049b391e7d
Marta Gonzalez Morales <pythonisamgm@gmail.com>
2024-07-24 12:23:57 +0200

    feat: add functionallity to the tests

--- src/test/java/com/CarePets/servicesTest/AppointmentServiceTest.java
+++ src/test/java/com/CarePets/servicesTest/AppointmentServiceTest.java
@@ -16,6 +16,7 @@ import org.mockito.MockitoAnnotations;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
@@ -50,15 +51,18 @@ class AppointmentServiceTest {
         appointmentList.add(ap1);
         appointmentList.add(ap2);
         when(iAppointmentRepository.findAll()).thenReturn(appointmentList);
-
+        when(iAppointmentRepository.findByTypeConsult("urgent")).thenReturn(ap1);
 
         //act
         ArrayList<Appointment> result = appointmentService.getAllAppointments();
+        Appointment result2 = appointmentService.getAppointmentByType("urgent");
+
 
         //assert
         assertNotNull(result);
         assertEquals(2, result.size());
         assertEquals("urgent", result.get(0).getTypeConsult());
+        assertEquals("urgent", result2.getTypeConsult());
 
     }
     @Test
@@ -73,15 +77,18 @@ class AppointmentServiceTest {
         appointmentList.add(ap1);
         appointmentList.add(ap2);
         when(iAppointmentRepository.findAll()).thenReturn(appointmentList);
-
+        when(iAppointmentRepository.findByName("jaro")).thenReturn(ap2);
 
         //act
         ArrayList<Appointment> result = appointmentService.getAllAppointments();
+        Appointment result2 = appointmentService.getAppointmentByName(jaro);
+
 
         //assert
         assertNotNull(result);
         assertEquals(2, result.size());
         assertEquals("jaro", result.get(1).getPet().getName());
+        assertEquals("jaro", result2.getPet().getName());
 
     }
     @Test
@@ -96,15 +103,18 @@ class AppointmentServiceTest {
         appointmentList.add(ap1);
         appointmentList.add(ap2);
         when(iAppointmentRepository.findAll()).thenReturn(appointmentList);
+        when(iAppointmentRepository.findByStatus("past")).thenReturn(ap2);
 
 
         //act
         ArrayList<Appointment> result = appointmentService.getAllAppointments();
+        Appointment result2 = appointmentService.getAppointmentByStatus("past");
 
         //assert
         assertNotNull(result);
         assertEquals(2, result.size());
         assertEquals("pending", result.get(0).getStatus());
+        assertEquals("past", result2.getStatus());
 
     }
     @Test
@@ -119,15 +129,18 @@ class AppointmentServiceTest {
         appointmentList.add(ap1);
         appointmentList.add(ap2);
         when(iAppointmentRepository.findAll()).thenReturn(appointmentList);
+        when(iAppointmentRepository.findById(1L)).thenReturn(Optional.of(ap1));
 
 
         //act
         ArrayList<Appointment> result = appointmentService.getAllAppointments();
+        Optional result2 = appointmentService.getAppointmentById(1L);
 
         //assert
         assertNotNull(result);
         assertEquals(2, result.size());
         assertEquals(1, result.get(0).getIdAppointment());
+        assertEquals(Optional.of(ap1), appointmentService.getAppointmentById(1L));
 
     }
     @Test
