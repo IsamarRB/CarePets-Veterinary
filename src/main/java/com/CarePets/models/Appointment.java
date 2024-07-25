0081505260bac76ede0462a75e875c3b471f18b8
Marta Gonzalez Morales <pythonisamgm@gmail.com>
2024-07-25 14:10:18 +0200

    refactor: remove extra line

--- src/main/java/com/CarePets/models/Appointment.java
+++ src/main/java/com/CarePets/models/Appointment.java
@@ -19,7 +19,6 @@ public class Appointment {
 
         @Id
         @GeneratedValue(strategy = GenerationType.IDENTITY)
-
         @Column(name = "id appointment")
         private Long idAppointment;
 
