fa3786d1ab8ba85e66bea16bba2d246d75175310
Marta Gonzalez Morales <pythonisamgm@gmail.com>
2024-07-26 10:28:27 +0200

    refactor: remove unnecessary methods

--- src/main/java/com/CarePets/repositories/IAppointmentRepository.java
+++ src/main/java/com/CarePets/repositories/IAppointmentRepository.java
@@ -3,8 +3,6 @@ package com.CarePets.repositories;
 import com.CarePets.models.Appointment;
 import org.springframework.data.repository.CrudRepository;
 
-import java.time.LocalDateTime;
-import java.util.List;
 
 public interface IAppointmentRepository extends CrudRepository<Appointment,Long> {
 
